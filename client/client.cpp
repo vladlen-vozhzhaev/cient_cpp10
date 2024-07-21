9e5cb2d8fa2a76880a458154974bf8cdafbf286f
Vladlen <vladlen@vozhzhaev.ru>
2024-07-21 13:52:28 +0300

    first commit

---
+++ client/client.cpp
@@ -0,0 +1,30 @@
+﻿#pragma comment(lib, "ws2_32.lib") // Этот код используется для автоматичкской связи с библиотекой ws2_32.lib
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // Отключение предупреждений
+#include <winsock2.h> // Включение файла winsock2.h для работы с функциями Winsock
+#include <iostream>
+
+SOCKET Connection;
+
+int main() {
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) { // Инициализируем Winsock
+		std::cout << "WSA ERROR" << std::endl;
+	}
+	else {
+		std::cout << "Client started!" << std::endl;
+	}
+
+	SOCKADDR_IN addr; // Адрес
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP_адрес к которому подключаетесь
+	addr.sin_port = htons(9123); // Порт к которому вы подключаетесь
+	addr.sin_family = AF_INET; // Семейство сети, в данном случае IPv4
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) {
+		std::cout << "ERROR: failed connect to server\n";
+		return 1;
+	}
+	std::cout << "Connected!\n";
+}
